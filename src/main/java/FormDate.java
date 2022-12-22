2305d264e2e25df8e44df07f31d7d1f80a85574b
Valeryi Dnkovtsev <valorapaulinio@gmail.com>
2022-12-22 13:43:02 +0300

    initial commit

---
+++ src/main/java/FormDate.java
@@ -0,0 +1,6 @@
+public class FormDate {
+    public String day;
+    public String month;
+    public String year;
+
+}
---
+++ src/main/java/Post.java
@@ -0,0 +1,15 @@
+public class Post {
+    public String name;
+    public String passport;
+    public String patronymic;
+    public String phone;
+    public String surname;
+    public String subscription;
+    public FormDate birthday;
+
+
+
+
+    }
+
+
