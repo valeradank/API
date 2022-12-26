789d3abf5b9eed59eae33f2a7fbb7e9ceaefac21
Valeryi Dnkovtsev <valorapaulinio@gmail.com>
2022-12-26 12:22:06 +0300

    change types

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,11 +7,11 @@ public static void main(String[] args) {
     post.patronymic ="Иванович";
     post.phone ="+7 (999)-999-99-99";
     post.surname ="Иванов";
-    post.subscription ="true";
+    post.subscription = true;
     post.birthday = new FormDate();
-    post.birthday.day = "13";
-    post.birthday.month = "6";
-    post.birthday.year = "1999";
+    post.birthday.day = 13;
+    post.birthday.month = 6;
+    post.birthday.year = 1999;
 
 }
 }
